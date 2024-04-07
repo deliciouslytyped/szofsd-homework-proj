977b3ce4262cef7e2555ba80c27c0eb06a7cdc2b
user <email>
2024-04-07 16:34:34 +0200

    build: javafx:run works with minimal 3D JavaFX demo

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -1,7 +1,11 @@
 package game;
 
+import javafx.application.Application;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hell world!");
+    // See the developer notes.
+    System.setProperty("prism.forceGPU", "true");
+    Application.launch(GameApplication.class, args);
   }
 }
